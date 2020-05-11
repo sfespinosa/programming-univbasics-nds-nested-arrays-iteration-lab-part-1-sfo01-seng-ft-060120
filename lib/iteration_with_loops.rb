fb0638ca5d2823c343ed41bac07eeba4ef7a4871
Scott Espinosa <scottjames.espinosa@gmail.com>
2020-05-11 05:27:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,11 +6,11 @@ def find_even_values(src)
   while row_index < src.length do 
     element_index = 0 
     while element_index < src[row_index].length do 
-      if src[row_index][element_index] % 2 = 0 
+      if src[row_index][element_index] % 2 == 0 
         p src[row_index][element_index]
       end 
       element_index += 1 
     end 
-    
+    row_index += 1 
   end 
 end
